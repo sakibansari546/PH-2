6aae37624c6c8fb834c87ecc2ff8db18112f013a
Sakib ANsari <coclovar9878@gmail.com>
2024-07-18 20:19:42 +0530

    ss

--- code/script.js
+++ code/script.js
@@ -540,3 +540,18 @@ for (let i = 1; i <= totalSections; i++) {
 }
 
 addImageScrollAnimation();
+
+
+function cursor (){
+    const blob = document.querySelector('#blob')
+    console.log(blob);
+    document.body.onpointermove = event=>{
+        const {clientX, clientY} = event;
+
+       blob.animate({
+        left : `${clientX}px`,
+        top : `${clientY}px`,
+       },{ duration:3000,fill: 'forwards'})
+    }
+}
+cursor()
